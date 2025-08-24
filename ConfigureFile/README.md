bc7e091bc3c833fd7a93678e967774602def492c
mauryask <mauryask1732@gmail.com>
2025-08-24 16:56:15 +0000

    Updates

---
+++ ConfigureFile/README.md
@@ -0,0 +1,3 @@
+This project deals with the auto-generation of the header file
+It generates a configuration file called configuration.hpp 
+it contains project name, project version and version (major, minor and patch)
--- ConfigureFile/app/main.cpp
+++ ConfigureFile/app/main.cpp
@@ -7,6 +7,9 @@ int main()
 {
     cout << project_name << endl;
     cout << project_version << endl;
+    cout << project_version_major << endl;
+    cout << project_version_minor << endl;
+    cout << project_version_patch << endl;
     print_hello_world();
     return 0;
 }
